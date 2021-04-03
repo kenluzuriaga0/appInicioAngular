b12417c26b16d2cd7def7d1dbc2aaaa447f1d9b3
Proyectos <kenluzuriaga0@hotmail.com>
2021-04-03 02:15:22 -0500

    Event binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -23,21 +23,37 @@ export class EmpleadoComponent implements OnInit {
     return this.edad;
   }
 
-  llamaEmpresa(empresa:string){
+  llamaEmpresa(empresa: string) {
 
   }
 
-/* propiedades para binding */
-  private activadoProperty=true;
+  /* propiedades para binding */
+  private activadoProperty = false;
   private registrado = false;
 
 
-getRegistrado(){
-  return this.registrado;
-}
-getActivadoProperty(){
-  return this.activadoProperty;
-}
+  getRegistrado() {
+    return this.registrado;
+  }
+  getActivadoProperty() {
+    return this.activadoProperty;
+  }
+  /* ----------- Event Binding ------------------  */
+  textodeRegistro = "no hay nadie registrado";
+
+  evento() {
+    this.textodeRegistro = "Usuario Registrado";
+  }
+
+  radioRegistro="";
+  setUsuarioRegistro(event: Event) {
+    //alert(event.target);
+    if ((<HTMLInputElement>event.target).value == "Si") {  //casting del input
+      this.radioRegistro = "El usuario SI se registra";
+    } else {
+      this.radioRegistro = "El usuario NO se registra";
+    } 
+  }
 
   constructor() { }
   ngOnInit(): void {

086c5487dd68550a0a98d49e459f9edc3d22e5bf
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2021-04-04 00:44:29 -0500

    calculadora + bootstrap

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent implements OnInit {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2
+  }
 
   constructor() { }
 
